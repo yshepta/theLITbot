62557df5e710e7e6eb5f717a48ebefd243ac0f53
yshepta <yuriy.shepta@gmail.com>
2016-10-28 21:37:59 -0500

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,4 +1,7 @@
 <?php 
+	//when you use this rename it to index.pph youll need xampp and you will 
+	//need to make a folder under xampp/htdocs called litbot and put this file inside of it 
+	//and when you run it in chrome type in localhost/litbot as the url 
 	include_once 'simple_html_dom.php';
 	//put in the url here 
 	$target_url = "https://www.google.com/search?site=&source=hp&q=guap&oq=guap&gs_l=hp.3..0l10.3414.6111.0.6350.11.9.2.0.0.0.149.961.3j6.9.0....0...1c.1.64.hp..0.9.802.0..46j0i131k1j0i46k1j0i131i155k1.gZKXbrDiOas";
